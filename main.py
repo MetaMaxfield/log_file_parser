4c29c29f75f4cd5523119dc682e7433c81170ab0
MetaMaxfield <metamaxfield@gmail.com>
2025-07-30 19:15:49 +0400

    feature: Add CLI options parser
    Add AVERAGE_REPORT_NAME
    Add get_command_line_options() with argparse:
    - Added required argument --file (-f) with multiple values
    - Added optional argument --report (-r) with choices and default value
    Add "_ = get_command_line_options()" in main()

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 """Parse a file with logs."""
 
+import argparse
+
+AVERAGE_REPORT_NAME = 'average'
+
+
+def get_command_line_options() -> argparse.Namespace:
+    """Return command line options."""
+    parser = argparse.ArgumentParser(description='Log file parser.')
+    parser.add_argument(
+        '-f', '--file', type=str, nargs='+', required=True, help='File to parse (possibly multiple values).'
+    )
+    parser.add_argument(
+        '-r',
+        '--report',
+        type=str,
+        choices=[
+            AVERAGE_REPORT_NAME,
+        ],
+        default=AVERAGE_REPORT_NAME,
+        help=f'Type report (default: {AVERAGE_REPORT_NAME}).',
+    )
+    args = parser.parse_args()
+    return args
+
 
 def main():
     """Execute the script step by step."""
+    _ = get_command_line_options()
     pass
 
 
